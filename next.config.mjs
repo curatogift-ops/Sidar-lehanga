e40388062ed1bc718df49b228fc8699f4cd92112
skadnan40605 <curatogift@gmail.com>
2025-12-06 21:38:24 +0530

    Update project files: fix build and config

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactCompiler: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.shopify.com',
+            },
+        ],
+    },
+    output: "standalone",
+};
+
+export default nextConfig;
